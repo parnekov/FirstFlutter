a8ac38197cce19f061163b85f8e2ed1a238dbd60
OleksandrParnekov <oleksandr.parnekov@gmail.com>
2018-12-14 17:53:15 +0200

    make structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
+import './product_manager.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-// This widget is the root of your application.
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,33 +11,7 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
             title: Text('Easy List'),
           ),
-          body: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      _products.add('Advanced Food Tester');
-                    });
-                  },
-                  child: Text('Add Product'),
-                ),
-              ),
-              Column(
-                children: _products
-                    .map((element) => Card(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset('assets/food.jpg'),
-                              Text(element)
-                            ],
-                          ),
-                        ))
-                    .toList(),
-              )
-            ],
-          )),
+          body: ProductManager()),
     );
   }
 }
